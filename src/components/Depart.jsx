e546c5d0115d74a2747b55fb308ef42f765966ab
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-11-19 13:35:43 +0100

    now we can get the data easily

--- src/components/Depart.jsx
+++ src/components/Depart.jsx
@@ -1,77 +1,86 @@
-import React, { useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useLocation } from "react-router-dom"
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+
 
 export const Depart = ({ getLocation }) => {
+  const location = useLocation();
 
-  const location = useLocation()
+  const [departData, setDepartData] = useState([]);
 
   const getDepart = async () => {
-
     try {
       const response = await axios.get(process.env.REACT_APP_DEPART);
-      console.log(response.data);
+      setDepartData(response.data);
     } catch (error) {
       console.error("Error:", error);
     }
-  }
+  };
 
   useEffect(() => {
-    getLocation(location)
-  }, [location])
+    getLocation(location);
+  }, [location]);
 
   useEffect(() => {
-    getDepart()
-  }, [])
+    getDepart();
+  }, []);
 
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
-          <TableRow >
-            <TableCell align="center" sx={{ fontWeight: "bold" }}>Date de depart et N° d'ordre annuel</TableCell>
-            <TableCell align="center" sx={{ fontWeight: "bold" }}>Date et N° de la lettre depart</TableCell>
-            <TableCell align="center" sx={{ fontWeight: "bold" }}>Désignation du destinataire</TableCell>
-            <TableCell align="center" sx={{ fontWeight: "bold" }}>Analyse de l'affaire</TableCell>
-            <TableCell align="center" sx={{ fontWeight: "bold" }}>Date et numéro de la réponse</TableCell>
+          <TableRow>
+            <TableCell align="center" sx={{ fontWeight: "bold" }}>
+              Date de depart et N° d'ordre annuel
+            </TableCell>
+            <TableCell align="center" sx={{ fontWeight: "bold" }}>
+              Date et N° de la lettre depart
+            </TableCell>
+            <TableCell align="center" sx={{ fontWeight: "bold" }}>
+              Désignation du destinataire
+            </TableCell>
+            <TableCell align="center" sx={{ fontWeight: "bold" }}>
+              Analyse de l'affaire
+            </TableCell>
+            <TableCell align="center" sx={{ fontWeight: "bold" }}>
+              Date et numéro de la réponse
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align="center" component="th" scope="row">
-                {row.name}
+          {departData.length <= 0 ? (
+            <TableRow>
+            <TableCell >
+                <h3 className="font-bold text-2xl p-4">Loading...</h3>
               </TableCell>
-              <TableCell align="center">{row.calories}</TableCell>
-              <TableCell align="center">{row.fat}</TableCell>
-              <TableCell align="center">{row.carbs}</TableCell>
-              <TableCell align="center">{row.protein}</TableCell>
             </TableRow>
-          ))}
+          ) : (
+            departData.map((depart) =>
+              depart.DepartTd.map((d) => (
+                <TableRow
+                    key={depart._id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="center">{d.number}</TableCell>
+                  <TableCell align="center">{d.messageDate}</TableCell>
+                  <TableCell align="center">{d.reciever}</TableCell>
+                  <TableCell align="center">{d.subject}</TableCell>
+                  <TableCell align="center">{d.answerdate}</TableCell>
+                </TableRow>
+              ))
+            )
+          )}
         </TableBody>
       </Table>
     </TableContainer>
   );
-}
+};
